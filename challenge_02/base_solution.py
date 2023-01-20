e67fbdd4cd3ee5bbc3f7bccf9f76380f68634b81
Samuel Hudec <samuel.hudec@allegro.pl>
2023-01-20 12:00:33 +0100

    - grammar correction - add base solution python script

--- challenge_02/base_solution.py
+++ challenge_02/base_solution.py
@@ -6,7 +6,6 @@ class SelfAttention(nn.Module):
     """
     One of the most popular part in NLP.
     If you understand self attention, you will understand the heard of transformers.
-    hint: https://www.youtube.com/watch?v=yGTUuEx3GkA
     """
     def __init__(self):
         super().__init__()
@@ -18,7 +17,7 @@ class SelfAttention(nn.Module):
         # (norm_attention * v).sum(-1)
         return
 
-
-x = torch.randint(0, 500, (10, 50))  # tokenizer(text)
-model = SelfAttention()
-model.forward(x)
+if __name__ == "__main__":
+    x = torch.randint(0, 500, (10, 50))  # tokenizer(text)
+    model = SelfAttention()
+    print(model.forward(x))

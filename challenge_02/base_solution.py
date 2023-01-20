ba5ecf83186c6d8decc257fe34b8c2c2fcfc13c7
Samuel Hudec <samuel.hudec@allegro.pl>
2023-01-20 16:48:34 +0100

    - first draft

--- challenge_02/base_solution.py
+++ challenge_02/base_solution.py
@@ -7,17 +7,26 @@ class SelfAttention(nn.Module):
     One of the most popular part in NLP.
     If you understand self attention, you will understand the heard of transformers.
     """
-    def __init__(self):
+    def __init__(self, token_length: int) -> None:
         super().__init__()
-        self.W = None
+        self.token_length = token_length
+        self.TW = self._trainable_weights()
 
-    def forward(self, x):
-        q, k, v = self.W
-        # norm_attention = F.softmax(q*k)
-        # (norm_attention * v).sum(-1)
-        return
+    def _trainable_weights(self):
+        k = nn.Linear(self.token_length, self.token_length)
+        q = nn.Linear(self.token_length, self.token_length)
+        v = nn.Linear(self.token_length, self.token_length)
+        return q, k, v
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # Scaled Dot-Product Attention
+        q, k, v = self.TW
+        s = torch.matmul(q(x), torch.t(k(x)))
+        w = nn.functional.normalize(s)
+        return torch.matmul(w, v(x))
 
 if __name__ == "__main__":
-    x = torch.randint(0, 500, (10, 50))  # tokenizer(text)
-    model = SelfAttention()
+    token_len = 50
+    x = torch.randint(0, 500, (10, token_len), dtype=torch.float32)  # tokenizer(text)
+    model = SelfAttention(token_len)
     print(model.forward(x))

1750e3d2fdf623638a016a392878a23be84122ca
Samuel Hudec <samuel.hudec@allegro.pl>
2023-01-21 17:33:24 +0100

    - add softmax

--- challenge_02/base_solution.py
+++ challenge_02/base_solution.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn.functional as F
 import torch.nn as nn
 
 
@@ -7,15 +8,15 @@ class SelfAttention(nn.Module):
     One of the most popular part in NLP.
     If you understand self attention, you will understand the heard of transformers.
     """
-    def __init__(self, token_length: int) -> None:
+    def __init__(self, embedding_length: int) -> None:
         super().__init__()
-        self.token_length = token_length
+        self.e_length = embedding_length
         self.TW = self._trainable_weights()
 
     def _trainable_weights(self):
-        k = nn.Linear(self.token_length, self.token_length)
-        q = nn.Linear(self.token_length, self.token_length)
-        v = nn.Linear(self.token_length, self.token_length)
+        k = nn.Linear(self.e_length, self.e_length)
+        q = nn.Linear(self.e_length, self.e_length)
+        v = nn.Linear(self.e_length, self.e_length)
         return q, k, v
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -23,10 +24,11 @@ class SelfAttention(nn.Module):
         q, k, v = self.TW
         s = torch.matmul(q(x), torch.t(k(x)))
         w = nn.functional.normalize(s)
+        w = F.softmax(w)
         return torch.matmul(w, v(x))
 
 if __name__ == "__main__":
-    token_len = 50
-    x = torch.randint(0, 500, (10, token_len), dtype=torch.float32)  # tokenizer(text)
-    model = SelfAttention(token_len)
+    embdd_len = 50
+    x = torch.randint(0, 500, (10, embdd_len), dtype=torch.float32)  # tokenizer(text)
+    model = SelfAttention(embdd_len)
     print(model.forward(x))
